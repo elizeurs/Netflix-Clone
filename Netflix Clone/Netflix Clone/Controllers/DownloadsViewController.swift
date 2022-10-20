751bc5065da40f36dfda00044abf969af288d213
Elizeu RS <13ders@gmail.com>
2022-10-20 20:04:18 -0300

    Creating MainTabBarViewController

---
+++ Netflix Clone/Netflix Clone/Controllers/DownloadsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DownloadsViewController.swift
+//  Netflix Clone
+//
+//  Created by Elizeu RS on 20/10/22.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//      view.backgroundColor = .systemBlue
+      view.backgroundColor = .systemBackground
+    }
+
+}
---
+++ Netflix Clone/Netflix Clone/Controllers/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  Netflix Clone
+//
+//  Created by Elizeu RS on 20/10/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//      view.backgroundColor = .systemRed
+      view.backgroundColor = .systemBackground
+
+      
+    }
+}
