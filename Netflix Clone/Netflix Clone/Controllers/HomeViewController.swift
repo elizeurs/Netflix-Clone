64a274adfbee47c29802d5eadf1aebab2f1d1693
Elizeu RS <13ders@gmail.com>
2022-10-23 16:30:31 -0300

    Customizing the navigation bar

--- Netflix Clone/Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Netflix Clone/Controllers/HomeViewController.swift
@@ -26,10 +26,25 @@ class HomeViewController: UIViewController {
       homeFeedTable.delegate = self
       homeFeedTable.dataSource = self
       
+      configureNavbar()
+      
 //      homeFeedTable.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
       let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
       homeFeedTable.tableHeaderView = headerView
     }
+  
+  private func configureNavbar() {
+    var image = UIImage(named: "netflixLogo")
+    image = image?.withRenderingMode(.alwaysOriginal)
+    navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .done, target: self, action: nil)
+
+    navigationItem.rightBarButtonItems = [
+      UIBarButtonItem(image: UIImage(systemName: "person"), style: .done, target: self, action: nil),
+      UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil)
+    ]
+    navigationController?.navigationBar.tintColor = .white
+//    navigationItem.rightBarButtonItem = UIBarButtonItem(image: image, style: .done, target: self, action: nil)
+  }
   
   override func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
@@ -66,6 +81,13 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
   // header height
   func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
     return 40
+  }
+  
+  func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    let defaultOffset = view.safeAreaInsets.top
+    let offset = scrollView.contentOffset.y + defaultOffset
+    
+    navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
   }
 }
 
