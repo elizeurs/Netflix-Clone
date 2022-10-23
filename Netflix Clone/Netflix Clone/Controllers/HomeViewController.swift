2c5c8d4d710ef84df1a14d98cd826433e4e28987
Elizeu RS <13ders@gmail.com>
2022-10-23 18:59:39 -0300

    Setting TableView sections titles

--- Netflix Clone/Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Netflix Clone/Controllers/HomeViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class HomeViewController: UIViewController {
   
+  let sectionTitles: [String] = ["Trending Movies", "Popular", "Trending Tv", "Upcoming Movies", "Top Rated"]
+  
   private let homeFeedTable: UITableView = {
     // grouped - tableView style the same as apple uses inside settings app.
     let table = UITableView(frame: .zero, style: .grouped)
@@ -55,7 +57,7 @@ class HomeViewController: UIViewController {
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
   
   func numberOfSections(in tableView: UITableView) -> Int {
-    return 20
+    return sectionTitles.count
   }
   
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -81,6 +83,18 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
   // header height
   func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
     return 40
+  }
+  
+  func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+    guard let header = view as? UITableViewHeaderFooterView else { return }
+      header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+      header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 20, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
+    header.textLabel?.textColor = .white
+    header.textLabel?.text = header.textLabel?.text?.lowercased()
+  }
+  
+  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return sectionTitles[section]
   }
   
   func scrollViewDidScroll(_ scrollView: UIScrollView) {
