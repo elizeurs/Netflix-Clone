4478b4052e891e076ec5e42f1d882d3a23c61850
Elizeu RS <13ders@gmail.com>
2022-10-25 18:17:21 -0300

    Viewing poster images inside CollectionViewCell

--- Netflix Clone/Netflix Clone/Views/TitleCollectionViewCell.swift
+++ Netflix Clone/Netflix Clone/Views/TitleCollectionViewCell.swift
@@ -33,8 +33,13 @@ class TitleCollectionViewCell: UICollectionViewCell {
   }
   
   public func configure(with model: String) {
+    guard let url = URL(string: "https://image.tmdb.org/t/p/w500/\(model)") else { return
+    }
+    
+    posterImageView.sd_setImage(with: url, completed: nil)
+    
 //    guard let url = URL(string: model) else { return }
 //    posterImageView.sd_setImage(with: url, completed: nil)
-    print(model)
+//    print(model)
   }
 }
