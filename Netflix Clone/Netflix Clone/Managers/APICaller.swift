cc147b7a94fd7b184e206db9b2df383d1be85e37
Elizeu RS <13ders@gmail.com>
2022-10-26 20:14:01 -0300

    Creating Top Search TableView inside TopSearch tab

--- Netflix Clone/Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Netflix Clone/Managers/APICaller.swift
@@ -130,4 +130,25 @@ class APICaller {
     }
     task.resume()
   }
+  
+  // GET /discover/movie
+  // https://api.themoviedb.org/3/discover/movie?api_key=<<api_key>>&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate
+  func getDiscoverMovies(completion: @escaping (Result<[Title], Error>) -> Void) {
+    guard let url = URL(string: "\(Constants.baseURL)/3/discover/movie?api_key=\(Constants.API_KEY)&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate") else { return }
+    let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+      guard let data = data, error == nil else {
+        return
+      }
+      
+      do {
+        let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
+        completion(.success(results.results))
+//        print(results)
+      } catch {
+        completion(.failure(APIError.failedToGetData))
+//        print(error.localizedDescription)
+      }
+    }
+    task.resume()
+  }
 }
